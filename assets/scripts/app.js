d00b7d0956f3e7dfced18f049dace0b399d47485
ASUS-REZERWOWY-\dawid <dpolanski1999@gmail.com>
2022-11-23 18:36:29 +0100

    initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,28 @@
+let mouseCursor = document.querySelector(".cursor");
+let navLink = document.querySelectorAll(".navbar-items li");
+let backDropText = document.querySelectorAll(".back-drop-text");
+
+window.addEventListener("mousemove", cursor);
+
+function cursor(e) {
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
+
+navLink.forEach((link) => {
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+  });
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+  });
+});
+
+backDropText.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("about-link");
+  });
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("about-link");
+  });
+});
