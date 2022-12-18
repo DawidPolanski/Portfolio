0ac8b325054049379e599a40787c52cc60975aa1
ASUS-REZERWOWY-\dawid <dpolanski1999@gmail.com>
2022-12-18 15:57:08 +0100

    add footer & fix some bugs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,10 @@ let mouseCursor = document.querySelector(".cursor");
 let navLink = document.querySelectorAll(".navbar-items");
 let backDropText = document.querySelectorAll(".back-drop-text");
 let hamburgercross = document.querySelectorAll(".burger-menu__line");
-let hambureger = document.querySelector(".burger-menu");
+let hamburger = document.querySelector(".burger-menu");
+let navigation = document.querySelectorAll(".navigation-menu");
+let portfolioImg = document.querySelectorAll(".portfolio-img");
+var clickcounter = 0;
 
 window.addEventListener("mousemove", cursor);
 
@@ -11,10 +14,21 @@ function cursor(e) {
   mouseCursor.style.left = e.clientX + "px";
 }
 
+portfolioImg.forEach((link) => {
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("cursorgrayscale");
+  });
+
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("cursorgrayscale");
+  });
+});
+
 navLink.forEach((link) => {
   link.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("link-grow");
   });
+
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
   });
@@ -24,19 +38,30 @@ backDropText.forEach((link) => {
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("about-link");
   });
+
   link.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("about-link");
   });
 });
 
-hambureger.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
   hamburgercross.forEach((link) => {
     link.classList.toggle("burgerline-active");
   });
 });
 
-// hambureger.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     hamburgercross.classList.add("burgerline-active");
-//   });
-// });
+hamburger.addEventListener("click", () => {
+  if (clickcounter == 0) {
+    navigation.forEach((link) => {
+      link.classList.add("open");
+      mouseCursor.classList.add("cursornav");
+      clickcounter++;
+    });
+  } else if (clickcounter == 1) {
+    navigation.forEach((link) => {
+      link.classList.remove("open");
+      mouseCursor.classList.remove("cursornav");
+      clickcounter--;
+    });
+  }
+});
